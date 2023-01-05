c3ea1121e357c89f6d93d0bcd294f03a17ed9885
Jikdo <jikdo_@outlook.com>
2023-01-05 11:26:41 +0000

    completed test

--- src/ERC20/test/ERC20.t.sol
+++ src/ERC20/test/ERC20.t.sol
@@ -42,4 +42,28 @@ contract ERC20Test is Test {
         assertEq(token.balanceOf(address(this)), fromBalanceBefore - 20e18);
 
     }
+
+    function testTransferFromAndApprove() public {
+        token.transfer(address(0), 40e18);
+        uint256 fromBalanceBefore = token.balanceOf(address(0));
+        uint256 toBalanceBefore = token.balanceOf(address(this));
+        vm.prank(address(0));
+        token.approve(address(this), 20e18);
+        token.transferFrom(address(0), address(this), 20e18);
+
+        assertEq(token.balanceOf(address(this)), toBalanceBefore + 20e18);
+        assertEq(token.balanceOf(address(0)), fromBalanceBefore - 20e18);
+
+    }
+
+    function testAllowance() public {
+        token.transfer(address(0), 40e18);
+        uint256 fromBalanceBefore = token.balanceOf(address(0));
+        uint256 toBalanceBefore = token.balanceOf(address(this));
+        vm.prank(address(0));
+        token.approve(address(this), 20e18);
+
+        assertEq(token.allowance(address(0), address(this)), 20e18);
+    } 
+
 }
